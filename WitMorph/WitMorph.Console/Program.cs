9a630192706491bf0ee902b6f9e21476e8cd5f90
Jason Stangroome <jason@codeassassin.com>
2013-04-24 10:06:37 -0700

    Use project as template instead of process template from Console

--- WitMorph/WitMorph.Console/Program.cs
+++ WitMorph/WitMorph.Console/Program.cs
@@ -8,16 +8,21 @@ namespace WitMorph.Console
     {
         static void Main(string[] args)
         {
-            Debug.Listeners.Add(new ConsoleTraceListener());
+            if (args.Length != 3)
+            {
+                System.Console.WriteLine("usage: {0} <collection uri> <project name to morph> <project name to use as template>",
+                    Process.GetCurrentProcess().ProcessName);
+                return;
+            }
 
             var collectionUri = new Uri(args[0]);
             var projectName = args[1];
-            var newProcessTemplateName = args[2];
+            var templateProjectName = args[2];
 
             var factory = new ProcessTemplateFactory();
 
             var currentTemplate = factory.FromActiveTeamProject(collectionUri, projectName);
-            var goalTemplate = factory.FromCollectionTemplates(collectionUri, newProcessTemplateName);
+            var goalTemplate = factory.FromActiveTeamProject(collectionUri, templateProjectName);
 
             var diffEngine = new DiffEngine(ProcessTemplateMap.ConvertScrum2ToAgile6());
             var differences = diffEngine.CompareProcessTemplates(currentTemplate, goalTemplate);
@@ -33,8 +38,6 @@ namespace WitMorph.Console
             //var filteredActions = actions.Where(a => !(a is DestroyWitdMorphAction)); // optionally skip deleting extra work item items
 
             engine.Apply(collectionUri, projectName, actions, Path.GetTempPath()); //TODO replace temp path with something useful
-
-            System.Console.WriteLine("DONE");
         }
     }
 }
