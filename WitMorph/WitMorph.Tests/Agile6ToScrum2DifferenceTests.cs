42468e1ecf9270dac532bf1dac080380b9aad232
Jesse Houwing <jesse.houwing@gmail.com>
2015-10-17 18:13:46 +0200

    Fixed a bug in state rename/consolidation when multiple states transition to the same target state.

--- WitMorph/WitMorph.Tests/Agile6ToScrum2DifferenceTests.cs
+++ WitMorph/WitMorph.Tests/Agile6ToScrum2DifferenceTests.cs
@@ -86,18 +86,18 @@ namespace WitMorph.Tests
         [TestMethod]
         public void Agile6ToScrum2Difference_should_identify_Closed_and_Resolved_states_renamed_to_Done_for_User_Story()
         {
-            var closedStateRename = Differences
-                .OfType<RenamedWorkItemStateDifference>()
-                .SingleOrDefault(d => d.CurrentWorkItemTypeName == "User Story"
-                    && d.CurrentStateName == "Closed"
-                    && d.GoalStateName == "Done");
-
             var resolvedStateRename = Differences
                 .OfType<RenamedWorkItemStateDifference>()
                 .SingleOrDefault(d => d.CurrentWorkItemTypeName == "User Story"
                     && d.CurrentStateName == "Resolved"
                     && d.GoalStateName == "Done");
 
+            var closedStateRename = Differences
+                .OfType<ChangedWorkItemStateDifference>()
+                .SingleOrDefault(d => d.CurrentWorkItemTypeName == "User Story"
+                    && d.CurrentStateName == "Closed"
+                    && d.GoalStateName == "Done");
+            
             Assert.IsNotNull(closedStateRename, "User Story Closed state rename not identified");
             Assert.IsNotNull(resolvedStateRename, "User Story Resolved state rename not identified");
         }
